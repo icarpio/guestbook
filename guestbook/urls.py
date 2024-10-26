b6f2503344667b39dbad6e00333d0c942e1b91d7
Ivan <icarpiodeveloper@gmail.com>
2024-10-26 19:04:12 +0200

    Fixes

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', include('guestbookapp.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Servir archivos multimedia en desarrollo y producción
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
