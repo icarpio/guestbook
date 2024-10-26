b79cc30b271b47577cb9416eb27ef49af0c860ba
Ivan <icarpiodeveloper@gmail.com>
2024-10-26 19:25:13 +0200

    Updates

--- guestbook/settings.py
+++ guestbook/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['guestbook-zam1.onrender.com']
 
 
 # Application definition
