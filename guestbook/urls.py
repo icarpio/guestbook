1a008ce6f882569f150570d90f4fbdca88261a71
Ivan <icarpiodeveloper@gmail.com>
2024-10-26 19:50:24 +0200

    Updates

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -2,16 +2,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.views.static import serve
-from django.urls import re_path
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('guestbookapp.urls')),
 ]
 
-urlpatterns += [
-    re_path(r'^media/(?P<path>.*)$', serve, {
-        'document_root': settings.MEDIA_ROOT,
-    }),
-]
+
+# Servir archivos multimedia en desarrollo y producción
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
